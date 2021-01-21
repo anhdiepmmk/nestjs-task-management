907a6cfb95e6cc3cdef85ec13215bf5cb74333e8
Pham ngoc diep <n08ni.dieppn@gmail.com>
2021-01-22 00:35:49 +0700

    Add auth module, service, repository.

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,36 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    // const exists = await this.findOne({ username });
+
+    // if (exists) {
+
+    // }
+
+    const user: User = this.create({
+      username,
+      password,
+    });
+
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        //duplicate username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
